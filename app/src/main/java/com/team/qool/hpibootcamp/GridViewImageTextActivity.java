81d3b5cf76b762a72b29e654150a62724f5dd7c4
parasmehta <pmehta.eu@googlemail.com>
2018-11-17 18:37:45 +0100

    initial commit

---
+++ app/src/main/java/com/team/qool/hpibootcamp/GridViewImageTextActivity.java
@@ -0,0 +1,42 @@
+package com.team.qool.hpibootcamp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class GridViewImageTextActivity extends AppCompatActivity {
+
+    GridView androidGridView;
+
+    String[] gridViewString = {
+            "Vision", "Hearing", "Memory", "Movement", "Attention", "Checkup"
+    } ;
+
+    int[] gridViewImageId = {
+            R.drawable.ic_launcher_background, R.drawable.ic_launcher_foreground,
+            R.drawable.ic_launcher_background, R.drawable.ic_launcher_foreground,
+            R.drawable.ic_launcher_background, R.drawable.ic_launcher_foreground
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gridview_image_text_example);
+
+        CustomGridViewActivity adapterViewAndroid = new CustomGridViewActivity(GridViewImageTextActivity.this, gridViewString, gridViewImageId);
+        androidGridView=(GridView)findViewById(R.id.grid_view_image_text);
+        androidGridView.setAdapter(adapterViewAndroid);
+        androidGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int i, long id) {
+                Toast.makeText(GridViewImageTextActivity.this, "GridView Item: " + gridViewString[+i], Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
