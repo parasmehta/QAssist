4c29b54fd07f1930b55bf3a86436408ab57d2262
parasmehta <pmehta.eu@googlemail.com>
2018-11-17 20:32:00 +0100

    changed icons, text

--- app/src/main/java/com/team/qool/hpibootcamp/GridViewImageTextActivity.java
+++ app/src/main/java/com/team/qool/hpibootcamp/GridViewImageTextActivity.java
@@ -5,27 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class GridViewImageTextActivity extends AppCompatActivity {
 
     GridView androidGridView;
+    ImageView imgView;
 
     String[] gridViewString = {
-            "Vision", "Hearing", "Memory", "Movement", "Attention", "Checkup"
+            "Hearing", "Vision", "Attention", "Memory", "Movement" , "Checkup"
     } ;
 
     int[] gridViewImageId = {
-            R.drawable.ic_launcher_background, R.drawable.ic_launcher_foreground,
-            R.drawable.ic_launcher_background, R.drawable.ic_launcher_foreground,
-            R.drawable.ic_launcher_background, R.drawable.ic_launcher_foreground
+            R.drawable.ic_hearing_black_48dp, R.drawable.ic_remove_red_eye_black_48dp,
+            R.drawable.ic_center_focus_weak_black_48dp, R.drawable.ic_settings_system_daydream_black_48dp,
+            R.drawable.ic_directions_walk_black_48dp, R.drawable.stethoscope
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gridview_image_text_example);
-
+//        imgView = (ImageView) findViewById(R.id.imageFavourites);
+//        imgView.setImageResource(R.drawable.ic_launcher_background);
         CustomGridViewActivity adapterViewAndroid = new CustomGridViewActivity(GridViewImageTextActivity.this, gridViewString, gridViewImageId);
         androidGridView=(GridView)findViewById(R.id.grid_view_image_text);
         androidGridView.setAdapter(adapterViewAndroid);
